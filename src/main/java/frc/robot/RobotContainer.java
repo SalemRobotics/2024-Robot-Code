2d7e64339262f91e8a33a503ebde3f84222334a6
frc6324-team-laptop <sau57robotics@gmail.com>
2024-01-13 09:03:22 -0500

    Copied Src and Vendordeps from the correct location

--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -4,55 +4,43 @@
 
 package frc.robot;
 
-import frc.robot.Constants.OperatorConstants;
-import frc.robot.subsystems.DriveTrain;
+import edu.wpi.first.math.MathUtil;
+import edu.wpi.first.wpilibj.XboxController;
+import edu.wpi.first.wpilibj.PS4Controller.Button;
 import edu.wpi.first.wpilibj2.command.Command;
-import edu.wpi.first.wpilibj2.command.InstantCommand;
-import edu.wpi.first.wpilibj2.command.button.CommandXboxController;
-import edu.wpi.first.wpilibj2.command.button.Trigger;
+import edu.wpi.first.wpilibj2.command.Commands;
+import edu.wpi.first.wpilibj2.command.RunCommand;
+import edu.wpi.first.wpilibj2.command.button.JoystickButton;
+import frc.robot.Constants.ControllerConstants;
+import frc.robot.subsystems.Drivetrain;
 
-/**
- * This class is where the bulk of the robot should be declared. Since Command-based is a
- * "declarative" paradigm, very little robot logic should actually be handled in the {@link Robot}
- * periodic methods (other than the scheduler calls). Instead, the structure of the robot (including
- * subsystems, commands, and trigger mappings) should be declared here.
- */
 public class RobotContainer {
-  // The robot's subsystems and commands are defined here...
-  final DriveTrain driveTrain = new DriveTrain();
-  // Replace with CommandPS4Controller or CommandJoystick if needed
-  private final CommandXboxController m_driverController =
-      new CommandXboxController(OperatorConstants.kDriverControllerPort);
+  
+  final XboxController mDriveController = new XboxController(ControllerConstants.kDriverPort); 
 
-  /** The container for the robot. Contains subsystems, OI devices, and commands. */
+  final Drivetrain mDrivetrain = new Drivetrain();
+
   public RobotContainer() {
-    // Configure the trigger bindings
     configureBindings();
-    driveTrain.setDefaultCommand(
-      driveTrain.arcadeDrive(m_driverController.getLeftY(), m_driverController.getLeftX())
+
+    mDrivetrain.setDefaultCommand(
+      new RunCommand(
+        () -> mDrivetrain.drive(
+          -MathUtil.applyDeadband(mDriveController.getLeftY(), ControllerConstants.kDriveDeadband),
+          -MathUtil.applyDeadband(mDriveController.getLeftX(), ControllerConstants.kDriveDeadband),
+          -MathUtil.applyDeadband(mDriveController.getRightX(), ControllerConstants.kDriveDeadband),
+          true, true), 
+        mDrivetrain)
     );
   }
 
-  /**
-   * Use this method to define your trigger->command mappings. Triggers can be created via the
-   * {@link Trigger#Trigger(java.util.function.BooleanSupplier)} constructor with an arbitrary
-   * predicate, or via the named factories in {@link
-   * edu.wpi.first.wpilibj2.command.button.CommandGenericHID}'s subclasses for {@link
-   * CommandXboxController Xbox}/{@link edu.wpi.first.wpilibj2.command.button.CommandPS4Controller
-   * PS4} controllers or {@link edu.wpi.first.wpilibj2.command.button.CommandJoystick Flight
-   * joysticks}.
-   */
   private void configureBindings() {
-
+    new JoystickButton(mDriveController, Button.kR1.value).whileTrue(
+      new RunCommand(() -> mDrivetrain.setX(), mDrivetrain)
+    );
   }
 
-  /**
-   * Use this to pass the autonomous command to the main {@link Robot} class.
-   *
-   * @return the command to run in autonomous
-  //  */
   public Command getAutonomousCommand() {
-    // An example command will be run in autonomous
-    return new InstantCommand();
+    return Commands.print("No autonomous command configured");
   }
 }
---
+++ src/main/java/frc/robot/subsystems/MAXSwerveModule.java
@@ -0,0 +1,205 @@
+// Copyright (c) FIRST and other WPILib contributors.
+// Open Source Software; you can modify and/or share it under the terms of
+// the WPILib BSD license file in the root directory of this project.
+
+package frc.robot.subsystems;
+
+import edu.wpi.first.math.geometry.Rotation2d;
+import edu.wpi.first.math.kinematics.SwerveModulePosition;
+import edu.wpi.first.math.kinematics.SwerveModuleState;
+import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
+
+import com.revrobotics.CANSparkMax;
+import com.revrobotics.CANSparkMaxLowLevel.MotorType;
+import com.revrobotics.SparkMaxAbsoluteEncoder.Type;
+import com.revrobotics.SparkMaxPIDController;
+import com.revrobotics.AbsoluteEncoder;
+import com.revrobotics.RelativeEncoder;
+
+import frc.robot.Constants.SwerveConstants;
+
+public class MAXSwerveModule {
+  final CANSparkMax mDrivingSparkMax;
+  final CANSparkMax mTurningSparkMax;
+
+  final RelativeEncoder mDrivingEncoder;
+  final AbsoluteEncoder mTurningEncoder;
+
+  final SparkMaxPIDController mDrivingPIDController;
+  final SparkMaxPIDController mTurningPIDController;
+
+  double drivingP=0.04, drivingI=0, drivingD=0, drivingFF=1 / SwerveConstants.kDriveWheelFreeSpeedRps, drivingMin=-1, drivingMax=1;
+  double turningP=1, turningI=0, turningD=0, turningFF=0, turningMin=-1, turningMax=1;
+
+  double mChassisAngularOffset = 0;
+  SwerveModuleState mDesiredState = new SwerveModuleState(0.0, new Rotation2d());
+
+  /**
+   * Constructs a MAXSwerveModule and configures the driving and turning motor,
+   * encoder, and PID controller. This configuration is specific to the REV
+   * MAXSwerve Module built with NEOs, SPARKS MAX, and a Through Bore
+   * Encoder.
+   */
+  public MAXSwerveModule(int drivingCANId, int turningCANId, double chassisAngularOffset) {
+    mDrivingSparkMax = new CANSparkMax(drivingCANId, MotorType.kBrushless);
+    mTurningSparkMax = new CANSparkMax(turningCANId, MotorType.kBrushless);
+
+    // Factory reset, so we get the SPARKS MAX to a known state before configuring
+    // them. This is useful in case a SPARK MAX is swapped out.
+    mDrivingSparkMax.restoreFactoryDefaults();
+    mTurningSparkMax.restoreFactoryDefaults();
+
+    // Setup encoders and PID controllers for the driving and turning SPARKS MAX.
+    mDrivingEncoder = mDrivingSparkMax.getEncoder();
+    mTurningEncoder = mTurningSparkMax.getAbsoluteEncoder(Type.kDutyCycle);
+    mDrivingPIDController = mDrivingSparkMax.getPIDController();
+    mTurningPIDController = mTurningSparkMax.getPIDController();
+    mDrivingPIDController.setFeedbackDevice(mDrivingEncoder);
+    mTurningPIDController.setFeedbackDevice(mTurningEncoder);
+
+    // Apply position and velocity conversion factors for the driving encoder. The
+    // native units for position and velocity are rotations and RPM, respectively,
+    // but we want meters and meters per second to use with WPILib's swerve APIs.
+    mDrivingEncoder.setPositionConversionFactor(SwerveConstants.kDrivingEncoderPositionFactor);
+    mDrivingEncoder.setVelocityConversionFactor(SwerveConstants.kDrivingEncoderVelocityFactor);
+
+    // Apply position and velocity conversion factors for the turning encoder. We
+    // want these in radians and radians per second to use with WPILib's swerve
+    // APIs.
+    mTurningEncoder.setPositionConversionFactor(SwerveConstants.kTurningEncoderPositionFactor);
+    mTurningEncoder.setVelocityConversionFactor(SwerveConstants.kTurningEncoderVelocityFactor);
+
+    // Invert the turning encoder, since the output shaft rotates in the opposite direction of
+    // the steering motor in the MAXSwerve Module.
+    mTurningEncoder.setInverted(SwerveConstants.kTurningEncoderInverted);
+
+    // Enable PID wrap around for the turning motor. This will allow the PID
+    // controller to go through 0 to get to the setpoint i.e. going from 350 degrees
+    // to 10 degrees will go through 0 rather than the other direction which is a
+    // longer route.
+    mTurningPIDController.setPositionPIDWrappingEnabled(true);
+    mTurningPIDController.setPositionPIDWrappingMinInput(SwerveConstants.kTurningEncoderPositionPIDMinInput);
+    mTurningPIDController.setPositionPIDWrappingMaxInput(SwerveConstants.kTurningEncoderPositionPIDMaxInput);
+
+    mDrivingSparkMax.setIdleMode(SwerveConstants.kDrivingMotorIdleMode);
+    mTurningSparkMax.setIdleMode(SwerveConstants.kTurningMotorIdleMode);
+    mDrivingSparkMax.setSmartCurrentLimit(SwerveConstants.kDrivingMotorCurrentLimit);
+    mTurningSparkMax.setSmartCurrentLimit(SwerveConstants.kTurningMotorCurrentLimit);
+
+    // Save the SPARK MAX configurations. If a SPARK MAX browns out during
+    // operation, it will maintain the above configurations.
+    mDrivingSparkMax.burnFlash();
+    mTurningSparkMax.burnFlash();
+
+    mChassisAngularOffset = chassisAngularOffset;
+    mDesiredState.angle = new Rotation2d(mTurningEncoder.getPosition());
+    mDrivingEncoder.setPosition(0);
+  }
+
+  /**
+   * Sends PID entries to Shuffleboard
+   * @param key String identifier to differentiate module groups
+   */
+  public void updateShuffleboardPID(String key) {
+    updateDrivingShuffleboardPID(key);
+    updateTurningShuffleboardPID(key);
+  }
+
+  /**
+   * Sends Driving PID entries to Shuffleboard
+   * @param key String identifier to differentiate module groups
+   */
+  void updateDrivingShuffleboardPID(String key) {
+    double tempP = SmartDashboard.getNumber("%s Driving Proportional Gain".formatted(key), drivingP);
+    double tempI = SmartDashboard.getNumber("%s Driving Integral Gain".formatted(key), drivingI);
+    double tempD = SmartDashboard.getNumber("%s Driving Derivative Gain".formatted(key), drivingD);
+    double tempFF = SmartDashboard.getNumber("%s Driving Feed Forward Gain".formatted(key), drivingFF);
+    double tempMin = SmartDashboard.getNumber("%s Driving Minimum Output".formatted(key), drivingMin);
+    double tempMax = SmartDashboard.getNumber("%s Driving Maximum Output".formatted(key), drivingMax);
+
+    if (tempP != drivingP) { mDrivingPIDController.setP(tempP); drivingP = tempP; }
+    if (tempI != drivingI) { mDrivingPIDController.setI(tempI); drivingI = tempI; }
+    if (tempD != drivingD) { mDrivingPIDController.setD(tempD); drivingD = tempD; }
+    if (tempFF != drivingFF) { mDrivingPIDController.setFF(drivingFF); drivingFF = tempFF; }
+    if ((tempMin != drivingMin) || (tempMax != drivingMax)) {
+      mDrivingPIDController.setOutputRange(tempMin, tempMax);
+      drivingMin = tempMin;
+      drivingMax = tempMax;
+    }
+  }
+
+  /**
+   * Sends Turning PID entries to Shuffleboard
+   * @param key String identifier to differentiate module groups
+   */
+  void updateTurningShuffleboardPID(String key) {
+    double tempP = SmartDashboard.getNumber("%s Turning Proportional Gain".formatted(key), turningP);
+    double tempI = SmartDashboard.getNumber("%s Turning Integral Gain".formatted(key), turningI);
+    double tempD = SmartDashboard.getNumber("%s Turning Derivative Gain".formatted(key), turningD);
+    double tempFF = SmartDashboard.getNumber("%s Turning Feed Forward Gain".formatted(key), turningFF);
+    double tempMin = SmartDashboard.getNumber("%s Turning Minimum Output".formatted(key), turningMin);
+    double tempMax = SmartDashboard.getNumber("%s Turning Maximum Output".formatted(key), turningMax);
+
+    if (tempP != turningP) { mTurningPIDController.setP(tempP); turningP = tempP; }
+    if (tempI != turningI) { mTurningPIDController.setI(tempI); turningI = tempI; }
+    if (tempD != turningD) { mTurningPIDController.setD(tempD); turningD = tempD; }
+    if (tempFF != turningFF) { mTurningPIDController.setFF(turningFF); turningFF = tempFF; }
+    if ((tempMin != turningMin) || (tempMax != turningMax)) {
+      mTurningPIDController.setOutputRange(tempMin, tempMax);
+      turningMin = tempMin;
+      turningMax = tempMax;
+    }
+  }
+
+  /**
+   * Returns the current state of the module.
+   *
+   * @return The current state of the module.
+   */
+  public SwerveModuleState getState() {
+    // Apply chassis angular offset to the encoder position to get the position
+    // relative to the chassis.
+    return new SwerveModuleState(mDrivingEncoder.getVelocity(),
+        new Rotation2d(mTurningEncoder.getPosition() - mChassisAngularOffset));
+  }
+
+  /**
+   * Returns the current position of the module.
+   *
+   * @return The current position of the module.
+   */
+  public SwerveModulePosition getPosition() {
+    // Apply chassis angular offset to the encoder position to get the position
+    // relative to the chassis.
+    return new SwerveModulePosition(
+        mDrivingEncoder.getPosition(),
+        new Rotation2d(mTurningEncoder.getPosition() - mChassisAngularOffset));
+  }
+
+  /**
+   * Sets the desired state for the module.
+   *
+   * @param desiredState Desired state with speed and angle.
+   */
+  public void setDesiredState(SwerveModuleState desiredState) {
+    // Apply chassis angular offset to the desired state.
+    SwerveModuleState correctedDesiredState = new SwerveModuleState();
+    correctedDesiredState.speedMetersPerSecond = desiredState.speedMetersPerSecond;
+    correctedDesiredState.angle = desiredState.angle.plus(Rotation2d.fromRadians(mChassisAngularOffset));
+
+    // Optimize the reference state to avoid spinning further than 90 degrees.
+    SwerveModuleState optimizedDesiredState = SwerveModuleState.optimize(correctedDesiredState,
+        new Rotation2d(mTurningEncoder.getPosition()));
+
+    // Command driving and turning SPARKS MAX towards their respective setpoints.
+    mDrivingPIDController.setReference(optimizedDesiredState.speedMetersPerSecond, CANSparkMax.ControlType.kVelocity);
+    mTurningPIDController.setReference(optimizedDesiredState.angle.getRadians(), CANSparkMax.ControlType.kPosition);
+
+    mDesiredState = desiredState;
+  }
+
+  /** Zeroes all the SwerveModule encoders. */
+  public void resetEncoders() {
+    mDrivingEncoder.setPosition(0);
+  }
+}
