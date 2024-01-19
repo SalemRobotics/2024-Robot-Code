eb1657d4bcfa13e356a4f5987ab168b064da4681
Gail-Bats08 <152564673+Gail-Bats08@users.noreply.github.com>
2024-01-18 19:52:34 -0500

    shooter WIP

--- src/main/java/frc/robot/subsystems/Shooter.java
+++ src/main/java/frc/robot/subsystems/Shooter.java
@@ -1,15 +1,23 @@
 package frc.robot.subsystems;
 import com.revrobotics.CANSparkMax;
 import com.revrobotics.CANSparkLowLevel.MotorType;
+
+import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 
 public class Shooter extends SubsystemBase {
-    CANSparkMax shooterSparkMaxOne = new CANSparkMax(0,MoterType.kBrushless);
-    CANSparkMax shooterSparkMaxTwo = new CANSparkMax(0,MoterType.kBrushless);
+    CANSparkMax shooterSparkMaxOne = new CANSparkMax(0,MotorType.kBrushless);
+    CANSparkMax shooterSparkMaxTwo = new CANSparkMax(0,MotorType.kBrushless);
 
     public Shooter() {
         shooterSparkMaxTwo.follow(shooterSparkMaxOne);
     }
 
-
+    public Command shootRing(double speed) {
+        return run(
+            () -> {
+                shooterSparkMaxOne.set(speed);
+            }
+        );
+    }
 }
