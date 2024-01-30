15005a123157aa78d7f14a1062ee7818d309f0a5
SophiaSell <81477144+frc6324-team-laptop@users.noreply.github.com>
2024-01-30 18:27:53 -0500

    Updated PathPlanner to new version and added odometery visualization in shuffleboard

--- src/main/java/frc/robot/subsystems/Drivetrain.java
+++ src/main/java/frc/robot/subsystems/Drivetrain.java
@@ -14,6 +14,7 @@ import edu.wpi.first.math.kinematics.SwerveModuleState;
 import edu.wpi.first.util.WPIUtilJNI;
 import edu.wpi.first.wpilibj.DriverStation;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
+import frc.robot.RobotContainer;
 import frc.robot.Constants.DriveConstants;
 import frc.util.SwerveUtils;
 
@@ -95,6 +96,8 @@ public class Drivetrain extends SubsystemBase {
             mRearLeft.getPosition(),
             mRearRight.getPosition()
         });
+
+    RobotContainer.m_field.setRobotPose(mOdometry.getPoseMeters());
   }
 
   /**
