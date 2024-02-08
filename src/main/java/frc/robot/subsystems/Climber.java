e5ae3349010d3a1e2427a7dd4510df32cc2cbc2c
Mason-to <81477144+frc6324-team-laptop@users.noreply.github.com>
2024-02-08 17:59:43 -0500

    added to climber subsystem

--- src/main/java/frc/robot/subsystems/Climber.java
+++ src/main/java/frc/robot/subsystems/Climber.java
@@ -1,7 +1,27 @@
 package frc.robot.subsystems;
 
+import com.revrobotics.CANSparkLowLevel.MotorType;
+import com.revrobotics.CANSparkMax;
+
+import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 
 public class Climber extends SubsystemBase {
-    
+    CANSparkMax climberMoter1 = new CANSparkMax(0,MotorType.kBrushless);
+    CANSparkMax climberMoter2 = new CANSparkMax(0,MotorType.kBrushless);
+
+    public Climber() {
+        climberMoter2.follow(climberMoter1);
+    }
+
+    public Command climbTime(double speed) {
+        return runEnd(
+            () -> {
+                climberMoter1.set(speed);
+            }, 
+            () -> {
+                climberMoter1.set(0);
+            }
+        );
+    }
 }
