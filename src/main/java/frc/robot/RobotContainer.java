77ab3c458f48e8b6946053a838e7827c222a7f15
JoeTheGoat <81477144+frc6324-team-laptop@users.noreply.github.com>
2024-02-08 19:37:25 -0500

    Finished the Non-Amp 5 note auto's general pathing

--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -7,6 +7,7 @@ package frc.robot;
 import com.pathplanner.lib.auto.AutoBuilder;
 import com.pathplanner.lib.auto.NamedCommands;
 
+
 import edu.wpi.first.math.MathUtil;
 import edu.wpi.first.wpilibj.XboxController;
 import edu.wpi.first.wpilibj.smartdashboard.Field2d;
