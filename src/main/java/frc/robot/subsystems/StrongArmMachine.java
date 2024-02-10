8f7914664aee674f5945c857e5e0084bb2ea0c58
SophiaSell <81477144+frc6324-team-laptop@users.noreply.github.com>
2024-02-10 08:43:59 -0500

    Something

---
+++ src/main/java/frc/robot/subsystems/StrongArmMachine.java
@@ -0,0 +1,10 @@
+package frc.robot.subsystems;
+
+import com.revrobotics.CANSparkMax;
+import com.revrobotics.CANSparkLowLevel.MotorType;
+
+import edu.wpi.first.wpilibj2.command.SubsystemBase;
+
+public class StrongArmMachine extends SubsystemBase {
+    final CANSparkMax mPivotMotor = new CANSparkMax(0, MotorType.kBrushless);
+}
