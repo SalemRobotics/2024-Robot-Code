d371e97c4fe526302fe2056e4a066bb55765b191
JoeTheGoat <81477144+frc6324-team-laptop@users.noreply.github.com>
2024-02-10 10:02:18 -0500

    SAM base
    needs actual commands set, should be ez pz

--- src/main/java/frc/robot/subsystems/StrongArmMachine.java
+++ src/main/java/frc/robot/subsystems/StrongArmMachine.java
@@ -3,8 +3,22 @@ package frc.robot.subsystems;
 import com.revrobotics.CANSparkMax;
 import com.revrobotics.CANSparkLowLevel.MotorType;
 
+import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 
 public class StrongArmMachine extends SubsystemBase {
     final CANSparkMax mPivotMotor = new CANSparkMax(0, MotorType.kBrushless);
+    final CANSparkMax mIndexMotor = new CANSparkMax(1, MotorType.kBrushless);
+
+    public Command runIndexer(){
+        return runEnd(
+            () -> {
+                
+            },
+            () -> {
+                
+            }
+        );
+
+    }
 }
