8e1e3d7efa46083ea66d7c289a98b5b01397b45f
JoeTheGoat <81477144+frc6324-team-laptop@users.noreply.github.com>
2024-02-10 15:29:18 -0500

    Update StrongArmMachine.java

--- src/main/java/frc/robot/subsystems/StrongArmMachine.java
+++ src/main/java/frc/robot/subsystems/StrongArmMachine.java
@@ -1,4 +1,5 @@
 package frc.robot.subsystems;
+import frc.robot.Constants.SAMConstants;
 
 import com.revrobotics.CANSparkMax;
 import com.revrobotics.CANSparkLowLevel.MotorType;
@@ -9,6 +10,7 @@ import edu.wpi.first.wpilibj2.command.SubsystemBase;
 public class StrongArmMachine extends SubsystemBase {
     final CANSparkMax mPivotMotor = new CANSparkMax(0, MotorType.kBrushless);
     final CANSparkMax mIndexMotor = new CANSparkMax(1, MotorType.kBrushless);
+    final ArmFeedForward feedForward = new ArmFeedForward(SAMConstants.kS, SAMConstants.kG, SAMConstants.kV, SAMConstants.kA);
 
     public Command runIndexer(){
         return runEnd(
