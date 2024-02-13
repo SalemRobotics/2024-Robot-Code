06eff6646bf34353d2a3c8a9c44e725a0bf5fa95
JoeTheGoat <81477144+frc6324-team-laptop@users.noreply.github.com>
2024-02-13 17:52:38 -0500

    Climber Spelling and Constants Fix

--- src/main/java/frc/robot/subsystems/Climber.java
+++ src/main/java/frc/robot/subsystems/Climber.java
@@ -2,6 +2,7 @@ package frc.robot.subsystems;
 
 import com.revrobotics.CANSparkLowLevel.MotorType;
 import com.revrobotics.CANSparkMax;
+import com.revrobotics.CANSparkBase.IdleMode;
 
 import frc.robot.Constants.ClimberConstants;
 
@@ -9,20 +10,25 @@ import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 
 public class Climber extends SubsystemBase {
-    CANSparkMax climberMoter1 = new CANSparkMax(ClimberConstants.kClimberMoter1CanID, MotorType.kBrushless);
-    CANSparkMax climberMoter2 = new CANSparkMax(ClimberConstants.kClimberMoter2CanID, MotorType.kBrushless);
+    final CANSparkMax climberMotor1 = new CANSparkMax(ClimberConstants.kClimberMotor1CanID, MotorType.kBrushless);
+    final CANSparkMax climberMotor2 = new CANSparkMax(ClimberConstants.kClimberMotor2CanID, MotorType.kBrushless);
 
     public Climber() {
-        climberMoter2.follow(climberMoter1);
+        climberMotor1.setIdleMode(IdleMode.kBrake);
+        climberMotor1.burnFlash();
+
+        climberMotor2.follow(climberMotor1);
+        climberMotor2.setIdleMode(IdleMode.kBrake);
+        climberMotor2.burnFlash();
     }
 
     public Command climbTime(double speed) {
         return runEnd(
             () -> {
-                climberMoter1.set(speed);
+                climberMotor1.set(speed);
             }, 
             () -> {
-                climberMoter1.stopMotor();
+                climberMotor1.stopMotor();
             }
         );
     }
