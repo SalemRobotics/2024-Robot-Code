d3575883fdf4f2e2a662275b4e7b3a9c026ed961
JoeTheGoat <81477144+frc6324-team-laptop@users.noreply.github.com>
2024-02-13 19:07:22 -0500

    more sam

--- src/main/java/frc/robot/subsystems/StrongArmMachine.java
+++ src/main/java/frc/robot/subsystems/StrongArmMachine.java
@@ -10,18 +10,50 @@ import edu.wpi.first.wpilibj2.command.SubsystemBase;
 
 public class StrongArmMachine extends SubsystemBase {
     final CANSparkMax mPivotMotor = new CANSparkMax(0, MotorType.kBrushless);
-    final CANSparkMax mIndexMotor = new CANSparkMax(1, MotorType.kBrushless);
+    final CANSparkMax mIntakeMotor = new CANSparkMax(1, MotorType.kBrushless);
     final ArmFeedforward feedForward = new ArmFeedforward(SAMConstants.kS, SAMConstants.kG, SAMConstants.kV, SAMConstants.kA);
 
-    public Command runIndexer(){
+    public Command runIntake() {
         return runEnd(
             () -> {
-                
+                mIntakeMotor.set(SAMConstants.speed);
             },
             () -> {
-                
+                mIntakeMotor.stop();
             }
         );
+    }
 
+    public Command pivotUp() {
+        return runEnd(
+            () -> {
+                mPivotMotor.set(-SAMConstants.speed);
+            },
+            () -> {
+                mPivotMotor.stop();
+            }
+        );
+    }
+
+    public Command pivotDown() {
+        return runEnd(
+            () -> {
+                mPivotMotor.set(SAMConstants.speed);
+            },
+            () -> {
+                mPivotMotor.stop();
+            }
+        );
+    }
+    
+    public Command runAmp() {
+        return runEnd(
+            () -> {
+                mIntakeMotor.set(-SAMConstants.speed);
+            },
+            () -> {
+                mIntakeMotor.stop();
+            }
+        );
     }
 }
