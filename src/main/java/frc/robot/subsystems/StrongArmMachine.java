e452dd6a0adc3afec4d874d59e7a420f825e936a
frc6324-team-laptop <81477144+frc6324-team-laptop@users.noreply.github.com>
2024-02-15 18:37:09 -0500

    make krakon

--- src/main/java/frc/robot/subsystems/StrongArmMachine.java
+++ src/main/java/frc/robot/subsystems/StrongArmMachine.java
@@ -1,6 +1,7 @@
 package frc.robot.subsystems;
 import frc.robot.Constants.SAMConstants;
 
+import com.ctre.phoenix6.hardware.TalonFX;
 import com.revrobotics.CANSparkMax;
 import com.revrobotics.CANSparkLowLevel.MotorType;
 
@@ -10,7 +11,7 @@ import edu.wpi.first.wpilibj2.command.SubsystemBase;
 
 public class StrongArmMachine extends SubsystemBase {
     final CANSparkMax mPivotMotor = new CANSparkMax(0, MotorType.kBrushless);
-    final CANSparkMax mIntakeMotor = new CANSparkMax(1, MotorType.kBrushless);
+    final TalonFX mIntakeMotor = new TalonFX(0);
     final ArmFeedforward feedForward = new ArmFeedforward(SAMConstants.kS, SAMConstants.kG, SAMConstants.kV, SAMConstants.kA);
 
     public Command runIntake() {
