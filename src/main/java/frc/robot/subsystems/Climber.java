160a3b53e6cf4955a62e699bb4b848cdded3c171
frc6324-team-laptop <sau57robotics@gmail.com>
2024-03-01 08:44:47 -0500

    very slight cleanup

--- src/main/java/frc/robot/subsystems/Climber.java
+++ src/main/java/frc/robot/subsystems/Climber.java
@@ -8,23 +8,25 @@ import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 
 public class Climber extends SubsystemBase {
-    final TalonFX climberMotor1 = new TalonFX(ClimberConstants.kClimberMotor1CanID);
-    final TalonFX climberMotor2 = new TalonFX(ClimberConstants.kClimberMotor2CanID);
+    final TalonFX mLeftMotor = new TalonFX(ClimberConstants.kClimberMotor1CanID);
+    final TalonFX mRightMotor = new TalonFX(ClimberConstants.kClimberMotor2CanID);
 
     public Climber() {
-        climberMotor1.setNeutralMode(NeutralModeValue.Brake);
+        mLeftMotor.setNeutralMode(NeutralModeValue.Brake);
 
-        climberMotor2.setControl(new Follower(ClimberConstants.kClimberMotor1CanID, false));
-        climberMotor2.setNeutralMode(NeutralModeValue.Brake);
+        mRightMotor.setControl(
+            new Follower(ClimberConstants.kClimberMotor1CanID, false)
+        );
+        mRightMotor.setNeutralMode(NeutralModeValue.Brake);
     }
 
     public Command climbTime(double speed) {
         return runEnd(
             () -> {
-                climberMotor1.set(speed);
+                mLeftMotor.set(speed);
             }, 
             () -> {
-                climberMotor1.stopMotor();
+                mLeftMotor.stopMotor();
             }
         );
     }
